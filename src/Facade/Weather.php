e5c1327328962ec95ec13ba295e7019f9ef53486
davis <130086230@qq.com>
2020-11-10 10:43:56 +0800

    up

---
+++ src/Facade/Weather.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fanxinyu
+ * Date: 2020-11-10
+ * Time: 10:19
+ */
+
+namespace Daviswwang\Weather;
+
+use Illuminate\Support\Facades\Facade;
+
+class Weather extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'weather';
+    }
+}
